cc4ca40f364a22d96380811ed96fbb0fba5e478a
jjmmwon <jmw1790@gmail.com>
2023-07-10 17:46:45 +0900

    [ADD] unsymmetric version

--- Q1/MC3/src/AM.js
+++ Q1/MC3/src/AM.js
@@ -1,10 +1,113 @@
+// import * as d3 from "d3";
+
+// export function AM() {
+//   const margin = { top: 70, right: 20, bottom: 20, left: 70 };
+//   const [width, height] = [
+//     800 - margin.left - margin.right,
+//     800 - margin.top - margin.bottom,
+//   ];
+//   const svg = d3
+//     .create("svg")
+//     .attr("width", width + margin.left + margin.right)
+//     .attr("height", height + margin.top + margin.bottom);
+
+//   function update(graph, oceanNodes) {
+//     const title = svg
+//       .append("g")
+//       .attr("transform", `translate(${width / 2}, ${margin.top / 2})`)
+//       .append("text")
+//       .attr("text-anchor", "middle")
+//       .text(`Graph ID: ${graph.id}`);
+
+//     const container = svg
+//       .append("g")
+//       .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+//     const nodes = graph.graph.nodes;
+//     const links = graph.graph.links;
+
+//     const xScale = d3
+//       .scaleBand()
+//       .padding(0.01)
+//       .domain(nodes.map((d) => d.id))
+//       .range([0, width]);
+//     const yScale = d3
+//       .scaleBand()
+//       .padding(0.01)
+//       .domain(nodes.map((d) => d.id))
+//       .range([0, height]);
+
+//     const xAxis = container
+//       .append("g")
+//       .attr("class", "x-axis")
+//       .attr("transform", `translate(${0}, ${0})`)
+//       .call(d3.axisTop(xScale));
+//     const yAxis = container
+//       .append("g")
+//       .attr("class", "y-axis")
+//       .attr("transform", `translate(${0}, ${0})`)
+//       .call(d3.axisLeft(yScale));
+
+//     let matrix = [];
+//     nodes.forEach((src) => {
+//       nodes.forEach((trg) => {
+//         matrix.push({
+//           source: src.id,
+//           target: trg.id,
+//           value: -1,
+//           ocean: false,
+//         });
+//       });
+//     });
+//     nodes.forEach((n, i) => {
+//       matrix[i * nodes.length + i].value = n.is_source;
+//       matrix[i * nodes.length + i].ocean = oceanNodes.includes(n.id);
+//       console.log(oceanNodes.includes(n.id));
+//     });
+//     links.forEach((l) => {
+//       let src = nodes.findIndex((n) => n.id === l.source);
+//       let trg = nodes.findIndex((n) => n.id === l.target);
+//       matrix[src * nodes.length + trg].value = l.type;
+//       matrix[trg * nodes.length + src].value = l.type;
+//     });
+
+//     const cells = container
+//       .append("g")
+//       .selectAll("rect")
+//       .data(matrix)
+//       .join("rect")
+//       .attr("x", (d) => xScale(d.source))
+//       .attr("y", (d) => yScale(d.target))
+//       .attr("width", xScale.bandwidth())
+//       .attr("height", yScale.bandwidth())
+//       .attr("fill", (d) =>
+//         d.value === "Beneficial Owner"
+//           ? "red"
+//           : d.value === "Company Contacts"
+//           ? "blue"
+//           : d.value === 1
+//           ? "green"
+//           : d.value === 0
+//           ? "yellow"
+//           : "white"
+//       )
+//       .attr("stroke", (d) => (d.ocean ? "black" : "none"))
+//       .attr("stroke-width", (d) => (d.ocean ? 8 : 0));
+//   }
+//   return {
+//     element: svg.node(),
+//     update,
+//   };
+// }
+
+// 우리 버전
 import * as d3 from "d3";
 
 export function AM() {
   const margin = { top: 70, right: 20, bottom: 20, left: 70 };
   const [width, height] = [
-    800 - margin.left - margin.right,
-    800 - margin.top - margin.bottom,
+    600 - margin.left - margin.right,
+    300 - margin.top - margin.bottom,
   ];
   const svg = d3
     .create("svg")
@@ -25,16 +128,18 @@ export function AM() {
 
     const nodes = graph.graph.nodes;
     const links = graph.graph.links;
+    const sourceNodes = nodes.filter((n) => n.is_source);
+    const targetNodes = nodes.filter((n) => !n.is_source);
 
     const xScale = d3
       .scaleBand()
       .padding(0.01)
-      .domain(nodes.map((d) => d.id))
+      .domain(targetNodes.map((d) => d.id))
       .range([0, width]);
     const yScale = d3
       .scaleBand()
       .padding(0.01)
-      .domain(nodes.map((d) => d.id))
+      .domain(sourceNodes.map((d) => d.id))
       .range([0, height]);
 
     const xAxis = container
@@ -49,35 +154,37 @@ export function AM() {
       .call(d3.axisLeft(yScale));
 
     let matrix = [];
-    nodes.forEach((src) => {
-      nodes.forEach((trg) => {
+    sourceNodes.forEach((s) => {
+      targetNodes.forEach((t) => {
         matrix.push({
-          source: src.id,
-          target: trg.id,
-          value: -1,
+          source: s.id,
+          target: t.id,
+          value: 0,
           ocean: false,
         });
       });
     });
-    nodes.forEach((n, i) => {
-      matrix[i * nodes.length + i].value = n.is_source;
-      matrix[i * nodes.length + i].ocean = oceanNodes.includes(n.id);
-      console.log(oceanNodes.includes(n.id));
-    });
+
+    // nodes.forEach((n, i) => {
+    //   matrix[i * targetNodes.length + i].value = n.is_source;
+    //   matrix[i * targetNodes.length + i].ocean = oceanNodes.includes(n.id);
+    //   console.log(oceanNodes.includes(n.id));
+    // });
+
     links.forEach((l) => {
-      let src = nodes.findIndex((n) => n.id === l.source);
-      let trg = nodes.findIndex((n) => n.id === l.target);
-      matrix[src * nodes.length + trg].value = l.type;
-      matrix[trg * nodes.length + src].value = l.type;
+      let src = sourceNodes.findIndex((n) => n.id === l.source);
+      let trg = targetNodes.findIndex((n) => n.id === l.target);
+      matrix[src * targetNodes.length + trg].value = l.type;
     });
+    console.log(matrix);
 
     const cells = container
       .append("g")
       .selectAll("rect")
       .data(matrix)
       .join("rect")
-      .attr("x", (d) => xScale(d.source))
-      .attr("y", (d) => yScale(d.target))
+      .attr("x", (d) => xScale(d.target))
+      .attr("y", (d) => yScale(d.source))
       .attr("width", xScale.bandwidth())
       .attr("height", yScale.bandwidth())
       .attr("fill", (d) =>
@@ -85,14 +192,8 @@ export function AM() {
           ? "red"
           : d.value === "Company Contacts"
           ? "blue"
-          : d.value === 1
-          ? "green"
-          : d.value === 0
-          ? "yellow"
           : "white"
-      )
-      .attr("stroke", (d) => (d.ocean ? "black" : "none"))
-      .attr("stroke-width", (d) => (d.ocean ? 8 : 0));
+      );
   }
   return {
     element: svg.node(),
