0c5e81dbba6443153cab34991d0ad976a18a31df
jjmmwon <jmw1790@gmail.com>
2023-07-11 18:04:11 +0900

    [ADD] Bipartite Graph

--- Q1/MC3/main.js
+++ Q1/MC3/main.js
@@ -1,6 +1,7 @@
 import "./style.scss";
 import * as d3 from "d3";
 import { AM } from "./src/AM";
+import { BP } from "./src/BP";
 import data from "./public/data/ocean_graphs.json";
 
 async function main() {
@@ -21,13 +22,18 @@ async function main() {
     );
     main.node().appendChild(am.element);
   });
-  // await d3
-  //   .csv(
-  //     "https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/heatmap_data.csv"
-  //   )
-  //   .then((data) => {
-  //     console.log(data);
-  //   });
+
+  // const main = d3.select("#app");
+  // const graphs = data.graphs;
+  //graphs.shift();
+  graphs.forEach((graph, i) => {
+    let bp = BP();
+    bp.update(
+      graph,
+      oceanNodes.map((n) => n.id)
+    );
+    main.node().appendChild(bp.element);
+  });
 }
 
 main();
