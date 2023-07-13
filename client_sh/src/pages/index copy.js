c05b0b751e77d072123af1043e01747918ffddeb
Seonghyeon Cho <67233988+st42597@users.noreply.github.com>
2023-07-13 14:07:51 +0900

    Donghee overview (#1)
    * [ADD] Datatable
    
    Reorganize the ordering of Overview and Detail view
    Chang the Overview using datatable
    
    * Update index.js
    
    ---------
    
    Co-authored-by: Donghee Hong <hdh12345@g.skku.edu>

--- client_sh/src/pages/index copy.js
+++ client_sh/src/pages/index copy.js
@@ -3,30 +3,28 @@ import Header from "../components/header";
 import OverviewMatrix from "../components/overviewMatrix";
 import Matrix from "../components/matrix";
 import Graph from "../data/graphs.json";
-
+import DataTable from "@/components/datatable";
 export default function Home() {
-  const numberOfOverview = 50;
-  const [selectedGraph, setSelectedGraph] = useState(1);
+  const [selectedGraph, setSelectedGraph] = useState(3);
   return (
-    <main className="h-full">
-      <Header />
-      <div className="flex h-full">
-        <div className="w-1/2 m-2 h-full">
-          <h1 className="text-xl">Overview</h1>
-          <div className="overflow-auto h-full">
-            {Graph.graphs.slice(0, numberOfOverview).map((x) => (
-              <div
-                className="w-1/2 hover:bg-sky-700 inline-block"
-                onClick={() => setSelectedGraph(x.id)}
-              >
-                <OverviewMatrix data={x}></OverviewMatrix>
-              </div>
-            ))}
+    <main className="h-full bg-gray-100">
+      <div className="block">
+        <Header />
+      </div>
+
+      <div className="flex-col h-full overflow-y-auto overflow-x-auto">
+        <div className="flex flex-col block m-2 h-1/3 p-10 overflow-x-auto">
+          <div className="w-full h-20 border-b-[1px] flex items-center text-xl">
+            <h1 className="text-xl">Overview</h1>
           </div>
+          <DataTable></DataTable>
         </div>
-        <div className="w-1/2 m-2">
-          <h1 className="text-xl">Detail</h1>
-          <div className="w-full">
+
+        <div className="flex flex-col block m-2 h-2/5 p-10">
+          <div className="w-full h-16 border-b-[1px] flex items-center text-xl">
+            <h1 className="text-xl">Detail</h1>
+          </div>
+          <div className="overflow-auto h-full">
             <div>
               <p>selectedGraphID: {selectedGraph}</p>
               <p>number of node: {Graph.graphs[selectedGraph - 1].num_nodes}</p>
