0186df509660505eb398747de42576835216effc
Seonghyeon Cho <67233988+st42597@users.noreply.github.com>
2023-07-14 21:47:43 +0900

    Donghee overview (#3)
    * [ADD] Datatable
    
    Reorganize the ordering of Overview and Detail view
    Chang the Overview using datatable
    
    * Update index.js
    
    ---------
    
    Co-authored-by: Donghee Hong <hdh12345@g.skku.edu>

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -3,8 +3,36 @@ import { useRouter } from "next/router";
 import OverviewMatrix from "../components/overviewMatrix";
 import Matrix from "../components/matrix";
 import Graph from "../data/graphs.json";
-
+import DataTable from "@/components/datatable";
 export default function Home() {
+<<<<<<< HEAD
+  const [selectedGraph, setSelectedGraph] = useState(3);
+  return (
+    <main className="h-full bg-gray-100">
+      <div className="block">
+        <Header />
+      </div>
+
+      <div className="flex-col h-full overflow-y-auto overflow-x-auto">
+        <div className="flex flex-col block m-2 h-1/3 p-10 overflow-x-auto">
+          <div className="w-full h-20 border-b-[1px] flex items-center text-xl">
+            <h1 className="text-xl">Overview</h1>
+          </div>
+          <DataTable></DataTable>
+        </div>
+
+        <div className="flex flex-col block m-2 h-2/5 p-10">
+          <div className="w-full h-16 border-b-[1px] flex items-center text-xl">
+            <h1 className="text-xl">Detail</h1>
+          </div>
+          <div className="overflow-auto h-full">
+            <div>
+              <p>selectedGraphID: {selectedGraph}</p>
+              <p>number of node: {Graph.graphs[selectedGraph - 1].num_nodes}</p>
+              <p>
+                number of edges: {Graph.graphs[selectedGraph - 1].num_edges}
+              </p>
+=======
   const router = useRouter();
   const numberOfOverview = 50;
   const [selectedGraph, setSelectedGraph] = useState(1);
@@ -22,6 +50,7 @@ export default function Home() {
               }}
             >
               <OverviewMatrix data={x}></OverviewMatrix>
+>>>>>>> main
             </div>
           ))}
         </div>
