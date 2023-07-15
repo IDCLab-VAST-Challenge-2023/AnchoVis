dc78ccec5183a7594a791000aeda1eb97c9b9fc5
SeongHyeon Cho <67233988+st42597@users.noreply.github.com>
2023-07-15 23:41:26 +0900

    feat: add hover design

--- client/src/components/datatable.js
+++ client/src/components/datatable.js
@@ -1,8 +1,16 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFish, faXmark } from "@fortawesome/free-solid-svg-icons";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
-export default function DataTable({ data, selectGraph, setSelectGraph }) {
+export default function DataTable({ data, selectedGraph, setSelectGraph }) {
+  const [hoveredGraph, setHoveredGraph] = useState(null);
+
+  function hoverSelectHandler(id) {
+    if (id === selectedGraph) return "rgb(148 163 184)";
+    else if (id === hoveredGraph) return "rgb(226 232 240)";
+    else return "white";
+  }
+
   return (
     <div className="flex flex-col overflow-y-auto">
       <div className="sm:-mx-6 lg:-mx-8">
@@ -14,7 +22,15 @@ export default function DataTable({ data, selectGraph, setSelectGraph }) {
                   <div className="px-6 py-4 w-full border-r">Network ID</div>
                 </th>
                 {data.map((x, i) => (
-                  <th scope="col" className="px-6 py-4 cursor-pointer hover:bg-slate-400" key={i} onClick={() => setSelectGraph(x.id)}>
+                  <th
+                    scope="col"
+                    className={"px-6 py-4 cursor-pointer"}
+                    style={{backgroundColor: hoverSelectHandler(x.id)}}
+                    key={i}
+                    onClick={() => setSelectGraph(x.id)}
+                    onMouseOver={() => setHoveredGraph(x.id)}
+                    onMouseDown={() => setHoveredGraph(null)}
+                  >
                     {x.id}
                   </th>
                 ))}
@@ -26,7 +42,15 @@ export default function DataTable({ data, selectGraph, setSelectGraph }) {
                   <div className="px-6 py-4 w-full border-r">Fishing</div>
                 </td>
                 {data.map((x, i) => (
-                  <td scope="col" className="px-6 py-4" key={i}>
+                  <td
+                    scope="col"
+                    className={"px-6 py-4 cursor-pointer"}
+                    style={{backgroundColor: hoverSelectHandler(x.id)}}
+                    key={i}
+                    onClick={() => setSelectGraph(x.id)}
+                    onMouseOver={() => setHoveredGraph(x.id)}
+                    onMouseDown={() => setHoveredGraph(null)}
+                  >
                     <FontAwesomeIcon icon={faFish} />{" "}
                     <FontAwesomeIcon icon={faXmark} /> {x.num_ocean_nodes}
                   </td>
@@ -41,7 +65,16 @@ export default function DataTable({ data, selectGraph, setSelectGraph }) {
                 {data.map((x, i) => {
                     let maxSimilarity = 0
                     x.graph.nodes.forEach((y) => { if (y.similarity > maxSimilarity) maxSimilarity = y.similarity })
-                    return <td className="whitespace-nowrap px-6 py-4" key={i}>{maxSimilarity}</td>
+                    return (
+                      <td
+                        className={"whitespace-nowrap px-6 py-4 cursor-pointer"}
+                        style={{backgroundColor: hoverSelectHandler(x.id)}}
+                        key={i}
+                        onClick={() => setSelectGraph(x.id)}
+                        onMouseOver={() => setHoveredGraph(x.id)}
+                        onMouseDown={() => setHoveredGraph(null)}
+                      >{maxSimilarity}</td>
+                    );
                   })
                 }
               </tr>
@@ -50,7 +83,14 @@ export default function DataTable({ data, selectGraph, setSelectGraph }) {
                   <div className="px-6 py-4 w-full border-r">Revenue</div>
                 </td>
                 {data.map((x, i) => (
-                    <td className="whitespace-nowrap px-6 py-4" key={i}>{x.average_revenue}</td>
+                    <td
+                      className={"whitespace-nowrap px-6 py-4 cursor-pointer"}
+                      style={{backgroundColor: hoverSelectHandler(x.id)}}
+                      key={i}
+                      onClick={() => setSelectGraph(x.id)}
+                      onMouseOver={() => setHoveredGraph(x.id)}
+                      onMouseDown={() => setHoveredGraph(null)}
+                    >{x.average_revenue}</td>
                   ))
                 }
               </tr>
@@ -59,7 +99,14 @@ export default function DataTable({ data, selectGraph, setSelectGraph }) {
                   <div className="px-6 py-4 w-full border-r"># of Nodes</div>
                 </td>
                 {data.map((x, i) => (
-                    <td className="whitespace-nowrap px-6 py-4" key={i}>{x.num_nodes}</td>
+                    <td
+                      className={"whitespace-nowrap px-6 py-4 cursor-pointer"}
+                      style={{backgroundColor: hoverSelectHandler(x.id)}}
+                      key={i}
+                      onClick={() => setSelectGraph(x.id)}
+                      onMouseOver={() => setHoveredGraph(x.id)}
+                      onMouseDown={() => setHoveredGraph(null)}
+                    >{x.num_nodes}</td>
                   ))
                 }
               </tr>
@@ -68,7 +115,14 @@ export default function DataTable({ data, selectGraph, setSelectGraph }) {
                   <div className="px-6 py-4 w-full border-r"># of Links</div>
                 </td>
                 {data.map((x, i) => (
-                    <td className="whitespace-nowrap px-6 py-4" key={i}>{x.num_links}</td>
+                    <td
+                      className={"whitespace-nowrap px-6 py-4  cursor-pointer"}
+                      style={{backgroundColor: hoverSelectHandler(x.id)}}
+                      key={i}
+                      onClick={() => setSelectGraph(x.id)}
+                      onMouseOver={() => setHoveredGraph(x.id)}
+                      onMouseDown={() => setHoveredGraph(null)}
+                    >{x.num_links}</td>
                   ))
                 }
               </tr>
