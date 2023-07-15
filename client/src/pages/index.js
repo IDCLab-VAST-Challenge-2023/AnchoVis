2130f9f2aee994a6a3f93f902592bd2a30270872
SeongHyeon Cho <67233988+st42597@users.noreply.github.com>
2023-07-15 23:47:06 +0900

    feat: consider unselected situation

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -5,7 +5,7 @@ import DataTableFilter from "@/components/dataTableFilter";
 import DetailTable from "@/components/detailTable";
 export default function Home() {
   const [graphOverviewData, setGraphOverviewData] = useState(Graph.graphs);
-  const [selectedGraph, setSelectedGraph] = useState(3);
+  const [selectedGraph, setSelectedGraph] = useState(null);
   const [networkFilter, setNetworkFilter] = useState({
     isFish: false,
     minSimilarity: 0,
@@ -34,17 +34,23 @@ export default function Home() {
       <div className="flex-col h-full overflow-auto">
         <div className="flex flex-col block m-2 h-fit p-10 overflow-x-auto">
           <div className="w-full h-20 border-b-[1px] flex items-center text-xl">
-            <h1 className="text-xl">Overview</h1>
+            <h1 className="text-xl ml-6">Overview</h1>
             <DataTableFilter setNetworkFilter={setNetworkFilter}></DataTableFilter>
           </div>
           <DataTable data={graphOverviewData} selectedGraph={selectedGraph} setSelectGraph={setSelectedGraph}></DataTable>
         </div>
         <div className="flex flex-col block m-2 h-full p-10">
           <div className="w-full h-20 border-b-[1px] flex items-center text-xl">
-            <h1 className="text-xl mr-6">Detail</h1>
+            <h1 className="text-xl mr-6 ml-6">Detail</h1>
             <DataTableFilter setNetworkFilter={setSourceFilter}></DataTableFilter>
           </div>
-          <DetailTable data={Graph.graphs[selectedGraph - 1]} filter={sourceFilter}></DetailTable>
+          {
+            selectedGraph !== null ?
+              <DetailTable data={Graph.graphs[selectedGraph - 1]} filter={sourceFilter}></DetailTable>
+              :
+              <div className="flex items-center justify-center w-full h-full p-24 text-2xl font-semibold">Please select a network</div>
+          }
+
         </div>
       </div>
     </main>
