04c293ba8c5c89b1194a0ff11490ced50470dec7
jason-choi-skku <jasonchoi3@g.skku.edu>
2023-07-18 15:20:16 +0900

    chore: refactor code

--- client/src/components/detailTable.js
+++ client/src/components/detailTable.js
@@ -10,59 +10,31 @@ const colorMap = {
 
 export default function detailTable({ data, filter }) {
   console.log("detailTable");
-  // console.log(data);
   const srcInfo = {};
   const src = {};
   const trg = {};
   const linkType = {};
-  for (const node of data.graph.nodes) {
-    srcInfo[node.id] = node;
-  }
-  for (const link of data.graph.links) {
-    if (
-      srcInfo[link.source].is_ocean >= 1 * filter.isFish &&
-      srcInfo[link.source].similarity >= filter.minSimilarity &&
-      srcInfo[link.source].total_revenue >= filter.minRevenue
-    ) {
-      if (link.source in src) src[link.source] += 1;
-      else src[link.source] = 1;
-      if (link.target in trg) trg[link.target] += 1;
-      else trg[link.target] = 1;
+  data.graph.nodes.forEach(node => srcInfo[node.id] = node);
+  data.graph.links.forEach(link => {
+    const source = srcInfo[link.source];
+    if (source.is_ocean >= 1 * filter.isFish && source.similarity >= filter.minSimilarity && source.total_revenue >= filter.minRevenue) {
+      src[link.source] = (src[link.source] || 0) + 1;
+      trg[link.target] = (trg[link.target] || 0) + 1;
       linkType[[link.source, link.target]] = link.type;
     }
-  }
-  let srcList = [];
-  let trgList = [];
-  for (const s in src) {
-    srcList.push([s, src[s]]);
-  }
-  for (const t in trg) {
-    trgList.push([t, trg[t]]);
-  }
-  srcList.sort((a, b) => b[1] - a[1]);
-  trgList.sort((a, b) => b[1] - a[1]);
-  srcList = srcList.filter((x) => x[1] > 0);
-  trgList = trgList.filter((x) => x[1] > 0);
+  });
+  let srcList = Object.entries(src).sort((a, b) => b[1] - a[1]).filter(x => x[1] > 0);
+  let trgList = Object.entries(trg).sort((a, b) => b[1] - a[1]).filter(x => x[1] > 0);
 
-  const matrixValue = [];
-  for (let i = 0; i < srcList.length; i++) {
-    const tmp = [];
-    for (let j = 0; j < trgList.length; j++) {
-      tmp.push({
-        type: linkType[[srcList[i][0], trgList[j][0]]],
-        src: srcList[i][0],
-        trg: trgList[j][0],
-      });
-    }
-    matrixValue.push(tmp);
-  }
+  const matrixValue = srcList.map((src) =>
+    trgList.map((trg) => ({
+      type: linkType[[src[0], trg[0]]],
+      src: src[0],
+      trg: trg[0],
+    }))
+  );
   function getSrcIndex(srcValue) {
-    for (let i = 0; i < matrixValue.length; i++) {
-      if (matrixValue[i].length > 0 && matrixValue[i][0].src === srcValue) {
-        return i;
-      }
-    }
-    return -1;
+    return matrixValue.findIndex((row) => row[0]?.src === srcValue);
   }
 
   return (
@@ -207,119 +179,4 @@ export default function detailTable({ data, filter }) {
       </Box>
     </Box>
   );
-
-  // return (
-  //   <div className="flex flex-col overflow-y-auto">
-  //     <div className="sm:-mx-6 lg:-mx-8">
-  //       <div className="inline-block py-2 sm:px-1 lg:px-8">
-  //         <table className="text-center text-sm font-light" layout="fixed">
-  //           <thead className="border-b font-medium dark:border-neutral-500">
-  //             <tr key="column_row">
-  //               <th
-  //                 scope="col"
-  //                 className="sticky left-0 bg-white z-10"
-  //                 key="sourceID"
-  //               >
-  //                 <div className="px-1 py-2 w-full border-r">Source ID</div>
-  //               </th>
-  //               <th scope="col" className="px-1 py-2" key="fishing">
-  //                 Fishing
-  //               </th>
-  //               {/* <th scope="col" className="px-1 py-2" key="revenue">
-  //                 Revenue
-  //               </th> */}
-  //               <th scope="col" className="px-1 py-2" key="similarity">
-  //                 Similarity
-  //               </th>
-  //               <th scope="col" className="px-1 py-2" key="country">
-  //                 Country
-  //               </th>
-  //               {trgList.slice(0, maxTrgSize).map((x, i) => (
-  //                 <th scope="col" className="px-1 py-2" key={`trg${i}`}>
-  //                   {x[0]}
-  //                 </th>
-  //               ))}
-  //             </tr>
-  //           </thead>
-  //           <tbody>
-  //             {srcList
-  //               .slice(0, maxSrcSize)
-  //               .filter((x) => srcInfo[x[0]].is_ocean >= 1 * filter.isFish)
-  //               .filter((x) => srcInfo[x[0]].similarity >= filter.minSimilarity)
-  //               .filter((x) => srcInfo[x[0]].total_revenue >= filter.minRevenue)
-  //               .map((x, i) => {
-  //                 x = srcInfo[x[0]];
-
-  //                 let idx = getSrcIndex(x.id);
-  //                 return (
-  //                   <tr
-  //                     className="border-b dark:border-neutral-500"
-  //                     key={`row${i}`}
-  //                   >
-  //                     <td
-  //                       className="whitespace-nowrap font-medium fixed ticky left-0"
-  //                       key={x.id}
-  //                     >
-  //                       <div className="px-1 py-2 w-full border-r">{x.id}</div>
-  //                     </td>
-  //                     <td
-  //                       scope="col"
-  //                       className="px-1 py-2"
-  //                       key={`is_ocean${i}`}
-  //                     >
-  //                       {x.is_ocean ? (
-  //                         <FontAwesomeIcon icon={faFish} />
-  //                       ) : (
-  //                         <FontAwesomeIcon icon={faXmark} />
-  //                       )}
-  //                     </td>
-  //                     {/* <td
-  //                       scope="col"
-  //                       className="px-1 py-2"
-  //                       key={`total_revenue${i}`}
-  //                     >
-  //                       {x.total_revenue}
-  //                     </td> */}
-  //                     <td
-  //                       scope="col"
-  //                       className="px-1 py-2 ticky"
-  //                       key={`similarity${i}`}
-  //                     >
-  //                       {x.similarity}
-  //                     </td>
-  //                     <td scope="col" className="px-1 py-2" key={`country${i}`}>
-  //                       {x.country}
-  //                     </td>
-  //                     {matrixValue[idx].map((y, j) => {
-  //                       function getColor(type) {
-  //                         if (type === "Beneficial Owner")
-  //                           return { background: "#4e79a7", color: "#ffffff" };
-  //                         else if (type === "Company Contacts")
-  //                           return { background: "#f28e2b", color: "#000000" };
-  //                         else
-  //                           return { background: "#f4f4f4", color: "#000000" };
-  //                       }
-  //                       const style = getColor(y.type);
-
-  //                       return (
-  //                         <td
-  //                           w={20}
-  //                           scope="col"
-  //                           className="px-1 py-2"
-  //                           style={style}
-  //                           key={`matrix${i}${j}`}
-  //                         >
-  //                           {/* {y.type} */}
-  //                         </td>
-  //                       );
-  //                     })}
-  //                   </tr>
-  //                 );
-  //               })}
-  //           </tbody>
-  //         </table>
-  //       </div>
-  //     </div>
-  //   </div>
-  // );
 }
