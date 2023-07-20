e5d400c28f11172c1ba23ae4d7d3f75b5082b712
jason-choi-skku <jasonchoi3@g.skku.edu>
2023-07-20 15:57:27 +0900

    chore: clean repository

--- client copy/src/components/header.js
+++
@@ -1,7 +0,0 @@
-export default function Header() {
-  return (
-    <header className="w-full h-16 border-b-[1px] flex items-center justify-center text-xl">
-      IDCLab VAST Challenge 2023
-    </header>
-  );
-}
